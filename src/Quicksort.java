e9617077ec68ac1986a05ce85b842602be98203e
Andrew Petriccione <apetricc@unca.edu>
2015-09-23 17:14:40 -0400

    Done. Comments, testing, might add a couple more arrays.

--- src/Quicksort.java
+++ src/Quicksort.java
@@ -1,82 +1,52 @@
 /**
- * Created by petriccione on 9/22/15.
+ * CSCI 333 Fall 2015, Professor Whitley
+ * Homework 4: Quicksort
+ * Created by Andrew Petriccione on 9/22/15.
+ * The goal of this homework is to create a class with a method using the quicksort algorithm to sort an array.
+ * To get quicksort to work we need another method called 'partition' which will return an index for the
+ * quicksort method to split the array into two sub-arrays, which quicksort can then overcome by calling itself
+ * recursively. After getting the quicksort method to work, we need to create another method very similar to
+ * quicksort called 'randomizedQuicksort' that uses a randomly selected index from the sub-array as the pivot.
  */
-
-
+import java.util.Arrays;
 import java.util.Random;
-
 /**
- *
-
-
- Do not use the Java Standard Library or other libraries, except you may use:
-
- console I/O
- java.util.Arrays.toString
- the java.util.Random class.
- You must implement your methods described above from scratch. Do not copy a solution from any person or third party
- source, but you may use the pseudocode in the textbook and the course webpage. Design and write the code yourself.
-
- Remember to comment your code! You should have a header comment with your name, date, class, assignment, and a brief
- description of what your code does. Read the syllabus carefully for details on all grading rubrics for coding homework.
-
-
+ * Quicksort is a class that has methods to sort the integers in an array using the quicksort algorithm.
  */
 public class Quicksort {
-
     /**
      * quicksort is a recursive method to sort integers in an array.
      * @param arr The array to be sorted.
-     * @param p  The pivot to sort the numbers around.
-     * @param r  The right boundary of the subarray to be sorted.
+     * @param p  The starting point (lower bounds) index of the sub-array to sort.
+     * @param r  The ending point (inclusive upper bounds) index of the array to be sorted.
      */
     public static void quicksort(int[] arr, int p, int r) {
         if (p < r) {
-          int q = partition(arr, p,r);  //split into 2 subproblems at q
+          int q = partition(arr, p,r);
             quicksort(arr,p,q-1);
             quicksort(arr,q+1,r);
         }
     }
-
     /**
      * arrayPrinter is a helper method to print the contents of an array of ints.
-     * @param arr
+     * @param arr The array to be printed.
      * @return a String that can be printed with System.out.print
      */
     public static String arrayPrinter(int[] arr){
-        System.out.print("\nThe array looks like: ");
         String result = "";
         for (int i = 0; i < arr.length; i++) {
             result += "[" + arr[i] + "]";
         }
         return result;
     }
-
     /**
-     * 24 points: You will need to define a private (helper) method called partition as in the slides/textbook, which is
-     invoked by quicksort. The parameters are the same as quicksort, but partition also returns an int index used by
-     quicksort to divide the two recursive subproblems.
-
-     x = A[r] // The value x is called the pivot value, chosen at index r
-     i = p - 1
-     for j = p to r - 1 // j is loop variable. Iterate through subarray.
-     if A[j] <= x // we find a “small” value less than the pivot x
-     i = i + 1
-     swap A[i] with A[j] // The “small” element goes left
-     swap A[i + 1] with A[r] // put pivot between left and right sides
-     return i + 1 // This is the index q in QUICKSORT
-
-
-     */
-
-    /**
-     * partition is a helper method for the sorting methods quicksort and randomizedQuicksort
-     * @param arr
-     * @param p
-     * @param r
+     * partition is a helper method for the sorting methods quicksort and randomizedQuicksort which functions
+     * to partition an array into smaller sub-arrays that can be sorted recursively.
+     * @param arr The array to partition.
+     * @param p   The starting point (lower bounds) index of the sub-array to partition.
+     * @param r   The ending point (upper bounds) index of the sub-array to partition.
      * @return
      */
-
     private static int partition(int[] arr, int p, int r) {
         int x = arr[r];
         int i = p - 1;
@@ -93,41 +63,18 @@ public class Quicksort {
         arr[r] = swap2;
         return i + 1;
     }
-
-    /**
-     * 12 points: Once you get your partition and quicksort methods working correctly, create a recursive method called
-     randomizedQuicksort as in the slides/textbook, which is extremely similar to quicksort above. Its parameters, return
-     value, and purpose are the same as the above quicksort method.  The only difference is the choice of pivot.  First,
-     swap a randomly selected element in the subarray with the rightmost element of the subarray, then proceed as above
-     to use the rightmost element as the pivot, by invoking partition and recursing as normal for quicksort. You may use
-     the the java.util.Random class as a random number generator.
-     *
-     *
-     *            if p < r  // base case is the implied empty else case
-                     // pivot choice can be randomized here
-                     q = PARTITION(A, p, r) // split 2 subproblems at q
-                     QUICKSORT(A, p, q - 1)
-                     QUICKSORT(A, q + 1, r)
-
-
-    Before we partition, let’s instead pick the pivot x randomly, as a random value in our subarray A[p...r], inclusive.
-    Randomize the pivot choice in the top of QUICKSORT’s recursive case, right before PARTITION, with this pseudocode:
-    z = RANDOM-INT(p, r) // random int number between p and r inclusive
-    swap A[z] with A[r]
-     */
-
     /**
      * randomizedQuicksort uses the quicksort algorithm with a randomly chosen pivot to sort an array of numbers.
      * @param arr The array to to perform randomizedQuicksort on.
-     * @param p  The starting point of the subarray to sort.
-     * @param r  The upper limit index of the subarray to sort.
+     * @param p  The starting point (lower bounds) index of the sub-array to sort.
+     * @param r  The ending point (upper bounds) index of the sub-array to sort.
      */
     public static void randomizedQuicksort(int[] arr, int p, int r) {
         if (p < r) {
             //int z = random b/t p & r
             Random myRand = new Random();
             int z = myRand.nextInt(r - p + 1) + p;
-            System.out.println("r is: " + r + " & p is: " + p + " & z is: " + z);
+           // System.out.println("r is: " + r + " & p is: " + p + " & z is: " + z);
             int swap = arr[z];
             arr[z] = arr[r] ;
             arr[r] = swap;
@@ -145,14 +92,31 @@ public class Quicksort {
          and the results into the console. This is the testing portion of the assignment.
          */
         int[] myArr1 = {5,4,3,2,1,11,34,19};
-        System.out.println(arrayPrinter(myArr1));
+        System.out.println("The array myArr1 starts like this: \n" + arrayPrinter(myArr1) + "\n");
+        int[] myArr1Copy = Arrays.copyOf(myArr1,myArr1.length);
+        System.out.println("The copy myArr1Copy looks like this: \n" +arrayPrinter(myArr1Copy) +"\n");
         quicksort(myArr1, 0, myArr1.length - 1);
-        System.out.println(arrayPrinter(myArr1));
+        System.out.println("After quicksort myArr1 looks like this: \n" + arrayPrinter(myArr1) + "\n");
+        randomizedQuicksort(myArr1Copy, 0, myArr1Copy.length - 1);
+        System.out.println("After randomizedQuicksort myArr1Copy looks like this: \n" + arrayPrinter(myArr1Copy) + "\n");
 
         int[] myArr2 = {5,4,3,2,1,0,11};
-        System.out.println(arrayPrinter(myArr2));
-        randomizedQuicksort(myArr2, 0, myArr2.length - 1);
-        System.out.println(arrayPrinter(myArr2));
+        int[] myArr2Copy = Arrays.copyOf(myArr2,myArr2.length);
+        System.out.println("The array myArr2 starts like this: \n" + arrayPrinter(myArr2) + "\n");
+        System.out.println("The copy myArr2Copy looks like this: \n" + arrayPrinter(myArr2Copy) + "\n");
+        quicksort(myArr2, 0, myArr2.length - 1);
+        System.out.println("After quicksort myArr2 looks like this: \n" + arrayPrinter(myArr2) + "\n");
+        randomizedQuicksort(myArr2Copy, 0, myArr2Copy.length - 1);
+        System.out.println("After randomizedQuicksort myArr2Copy looks like this: \n" + arrayPrinter(myArr2Copy) + "\n");
+
+        int[] myArr3 = {15,14,33,12,111,-30,110};
+        int[] myArr3Copy = Arrays.copyOf(myArr3,myArr3.length);
+        System.out.println("The array myArr3 starts like this: \n" + arrayPrinter(myArr3) + "\n");
+        System.out.println("The copy myArr3Copy looks like this: \n" + arrayPrinter(myArr3Copy) + "\n");
+        quicksort(myArr3, 0, myArr3.length - 1);
+        System.out.println("After quicksort myArr2 looks like this: \n" + arrayPrinter(myArr3) + "\n");
+        randomizedQuicksort(myArr3Copy, 0, myArr3Copy.length - 1);
+        System.out.println("After randomizedQuicksort myArr2Copy looks like this: \n" + arrayPrinter(myArr3Copy) + "\n");
 
 
 
